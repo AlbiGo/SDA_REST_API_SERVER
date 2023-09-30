991b14da9907dadd2061ab81cc5bc510a0da287c
Albi.Goxhaj <Albi.Goxhaj@fti.edu.al>
2023-09-30 09:55:05 +0200

    WIP

--- SDA_WEB_API/DataLayer/Models/Fatura.cs
+++ SDA_WEB_API/DataLayer/Models/Fatura.cs
@@ -1,8 +1,15 @@
-﻿namespace SDA_WEB_API.DataLayer.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SDA_WEB_API.DataLayer.Models
 {
     public class Fatura
     {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("Artikull")] //Same as Artikull in 12 row
         public int ArtikullID { get; set; }
+        public Artikull Artikull { get; set; }
         public int Sasia { get; set; }
         public decimal Cmimi { get; set; }
     }
